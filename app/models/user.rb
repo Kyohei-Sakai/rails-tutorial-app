6b405c7d6b060bf4a13423ad670a7ff5db58196f
Kyohei-Sakai <nico_f00tb@yahoo.co.jp>
2017-04-05 11:21:05 +0900

    add test of lower-case email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # self.email = self.email.downcase -> 右側のselfは省略可
-  before_save { self.email = email.downcase }
+  # before_save { self.email = email.downcase }
+  before_save { email.downcase! }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
