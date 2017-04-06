9eaf8bfa44b5282798ac894e950e4c36ce20f567
Kyohei-Sakai <nico_f00tb@yahoo.co.jp>
2017-04-06 14:33:09 +0900

    Add login helper for test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -28,6 +28,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     # POSTリクエストを送信した結果を見て、指定されたリダイレクト先に移動する
     follow_redirect!
     assert_template 'users/show'
+    assert is_logged_in?
     # flashに対するテスト -> わからん
     # assert_not flash.danger
   end
