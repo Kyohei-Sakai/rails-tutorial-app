b6151f68512a9e2c3247b4f2f72ec06cd3dc520a
Kyohei-Sakai <nico_f00tb@yahoo.co.jp>
2017-04-09 10:45:12 +0900

    Add update password validation

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,7 @@
 class PasswordResetsController < ApplicationController
   before_action :get_user,   only: [:edit, :update]
   before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
 
   def new
   end
@@ -21,7 +22,24 @@ class PasswordResetsController < ApplicationController
   def edit
   end
 
+  def update
+    if params[:user][:password].empty?                  # (3) への対応
+      @user.errors.add(:password, "can't be empty")
+      render 'edit'
+    elsif @user.update_attributes(user_params)          # (4) への対応
+      log_in @user
+      flash[:success] = "Password has been reset."
+      redirect_to @user
+    else
+      render 'edit'                                     # (2) への対応
+    end
+  end
+
   private
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
 
     def get_user
       @user = User.find_by(email: params[:email])
@@ -34,4 +52,20 @@ class PasswordResetsController < ApplicationController
         redirect_to root_url
       end
     end
+
+    # 有効なユーザーかどうか確認する
+def valid_user
+  unless (@user && @user.activated? &&
+          @user.authenticated?(:reset, params[:id]))
+    redirect_to root_url
+  end
+end
+
+# 期限切れかどうかを確認する
+def check_expiration
+  if @user.password_reset_expired?
+    flash[:danger] = "Password reset has expired."
+    redirect_to new_password_reset_url
+  end
+end
 end
